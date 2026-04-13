5801632997f773f1d9042b3cb4dc83e9b75f69f7
Alex Platz <5171713-oddx@users.noreply.gitlab.com>
2026-04-13 15:02:05 -0500

    Logging

--- apps/db/index.ts
+++ apps/db/index.ts
@@ -1,3 +1,25 @@
-import { getAllUsers } from './client'
-console.log(await getAllUsers())
-console.log('sqlite does not need a server. leaving structure in place for future dbs like surrealdb')
+import { getAllUsers } from './client/auth'
+import { configure, getConsoleSink, getLogger } from '@logtape/logtape'
+
+const isDevelopment = process.env.NODE_ENV === 'development'
+
+await configure({
+  sinks: {
+    console: getConsoleSink(),
+    // file: getFileSink(isDevelopment ? 'dev.log' : 'prod.log'),
+  },
+  loggers: [
+    {
+      category: 'template-db',
+      lowestLevel: isDevelopment ? 'trace' : 'info',
+      // odd formatting for symmetry across monorepo
+      // in a real app, we'd log to console in dev and remote file in prod
+      sinks: isDevelopment ? ['console'] : ['console'],
+    },
+    { category: ['logtape', 'meta'], sinks: ['console'], lowestLevel: 'warning' }
+  ],
+})
+
+const logger = getLogger(["template-db", "index"]);
+// logger.info`${await getAllUsers()}`
+logger.info`sqlite does not need a server. leaving structure in place for future dbs like surrealdb`
