067afbdc0e7eaf0d31d38a581f34bb39a18195e1
Alex Platz <me@oddx.xyz>
2026-04-21 08:52:49 -0500

    kinda working server

--- apps/client/serve.ts
+++ apps/client/serve.ts
@@ -1,15 +1,22 @@
+const PUBLIC_DIR = './dist'
+
 const server = Bun.serve({
   // port: Bun.env.VITE_CLIENT_URL,
   port: 3001,
-  fetch: (req) => {
+  fetch: async (req) => {
     const url = new URL(req.url)
 
     const path = url.pathname === '/' ?
       '/index.html' :
       url.pathname
 
-    const file = Bun.file(`./dist${path}`)
-    return new Response(file)
+    const file = Bun.file(`${PUBLIC_DIR}${path}`)
+
+    if (await file.exists()) {
+      return new Response(file);
+    } else {
+      return new Response(`${PUBLIC_DIR}/index.html`)
+    }
   },
 });
 
