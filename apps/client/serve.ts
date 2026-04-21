8a16334b5db455198c9dbbdf0b02be0f6a127d3c
Alex Platz <me@oddx.xyz>
2026-04-21 13:48:15 -0500

    sufficiently working bun static file server

--- apps/client/serve.ts
+++ apps/client/serve.ts
@@ -1,24 +1,22 @@
-const PUBLIC_DIR = './dist'
-
 const server = Bun.serve({
-  // port: Bun.env.VITE_CLIENT_URL,
+  // port: Bun.env.VITE_CLIENT_URL!,
   port: 3001,
   fetch: async (req) => {
     const url = new URL(req.url)
+    let filePath = url.pathname
 
-    const path = url.pathname === '/' ?
-      '/index.html' :
-      url.pathname
+    // Default to index.html for SPAs
+    if (filePath === "/") filePath = "./index.html"
+    const file = Bun.file(`./dist${filePath}`)
 
-    const file = Bun.file(`${PUBLIC_DIR}${path}`)
+    // Serve file from dist
+    return new Response(
+      await file.exists() ?
+        file :
+        Bun.file('./dist/index.html')
+    )
+  }
+})
 
-    if (await file.exists()) {
-      return new Response(file);
-    } else {
-      return new Response(`${PUBLIC_DIR}/index.html`)
-    }
-  },
-});
-
-console.log(`Listening on ${server.url}`);
+console.log(`Listening on ${server.url}`)
 
